d88878a82b40673e459ccb5a28c960b41abb5bb6
Buford Eeds <bufordeeds8@gmail.com>
2019-08-01 18:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function logWhisper(string) {
   console.log ('hello');
 }
 
-function sayHiToGrandma(string) {
-  return string.toLowerCase()
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
 }
-
-'hello'
